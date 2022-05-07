5b0d3a21786c302c8c673319e5446818468544ad
Rocly Fierro <finnslighthouse@gmail.com>
2022-05-07 12:01:07 -0700

    pulling threejs objects to highlight clean website withou frills

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
 import Navbar from './navbar'
-import NoSsr from '../layouts/no-ssr'
 
 const Main = ({ children, router }) => {
     return (
